84a75bb7edd590a1d3bdbdf7b616bab0b99367be
dualphase90 <dvkaruna@asu.edu>
2016-11-23 16:56:44 -0600

    Some Flask Examples

--- flask_resouceful_routing.py
+++ flask_resouceful_routing.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request
 from flask_restful import Resource, Api
 
@@ -18,3 +19,18 @@ api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# How to  Run
+'''
+You can try it like this:
+
+$ curl http://localhost:5000/todo1 -d "data=Remember the milk" -X PUT
+{"todo1": "Remember the milk"}
+$ curl http://localhost:5000/todo1
+{"todo1": "Remember the milk"}
+$ curl http://localhost:5000/todo2 -d "data=Change my brakepads" -X PUT
+{"todo2": "Change my brakepads"}
+$ curl http://localhost:5000/todo2
+{"todo2": "Change my brakepads"}
+'''
---
+++ pythonic_data_science.py
@@ -0,0 +1,3 @@
+name='Deepak'
+age='26'
+
